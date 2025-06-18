138ecf63f11ee56ef4354ec529a90668540c6da5
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-18 13:35:40 +0900

    List / Set

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,24 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> fruits = new ArrayList<>();
+        List<String> listFruits = new ArrayList<>();
+        Set<String> fruits = new HashSet<>();
 
-        fruits.add("Cherry");
+        listFruits.add("Apple");
+        listFruits.add("Apple");
+
         fruits.add("Apple");
-        fruits.add("Bbanana");
-        fruits.add("Tomato");
-        fruits.add("Mango");
-        fruits.add("Egg plant");
-        fruits.add("Banana");
-        fruits.add("Potato");
-        fruits.add("Dragonfruit");
+        fruits.add("Apple");
 
-        List<String> subFruits = fruits.subList(0, 2);      // 0~(2-1) 인덱스
+        for (String listFruit: listFruits) {
+            System.out.println(listFruit);
+        }
 
-        for (String subFruit : subFruits){
-            System.out.println(subFruit);
+        System.out.println();
+
+        for (String fruit:fruits) {
+            System.out.println(fruit);
         }
     }
 }
